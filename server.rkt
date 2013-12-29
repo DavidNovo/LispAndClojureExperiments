0de5d5ebf0542819b7208206d119ab1c4970e992
david.novogrodsky <david.novogrodsky@gmail.com>
2013-12-29 14:02:42 -0600

    adding some testing functions to the top level :

--- server.rkt
+++ server.rkt
@@ -22,8 +22,9 @@
 
 ;; constants section
 (define port-no  8080) ;HTTP port of server
-
-
+(define input-stream "place holder for a stream" )
+(define number-of-tcp-connections 5)
+(define sample-http-request "GET /path/to/file/index.html HTTP/1.0 \r\n")
 
 ;; variable section
 
@@ -44,14 +45,55 @@
 
 ;; tests
 (check-expect (string? hello-world-page) true)
-
 (check-expect (number? port-no) true)
 (check-expect (= port-no 8080) true)
 
 ;; functions
 
+;; first a method to serve pages, to accept a TCP connection
+;; this is the top-level function
+(define (serve port-no)
+  (define listener (tcp-listen port-no number-of-tcp-connections #t ))
+    (define (loop)
+      (accept-and-handle listener)
+      (loop))
+    (loop))
+
+
+;; !!
+;; This is a listener for the serve function, listens for TCP connections
+;; port number, number of connection, allow for reuse
+;; number, number, boolean -> TCP listener
+;;(define listener (tcp-listen port-no number-of-tcp-connections #t ))
+;; when I moved this into the server function definition it worked, re-served pages a lot.  why?
+
+;; examples
+;;(check-expect (listener (tcp-listen port-no number-of-tcp-connections #t "127.0.0.1")) #<tcp-listener>)
+
+;; !!
+;; accepts a TCP connection ->returns response
+;;(define (accept-and-handle listener)
+;;  ( listener))
+(define (accept-and-handle listener)
+  (define-values (input-stream output-stream)(tcp-accept listener))
+    (handle-tcp input-stream output-stream)
+    (close-input-port input-stream)
+    (close-output-port output-stream))
+;; examples
+;; this should return a hello world page
+;;(check-expect (accept-and-handle listener) hello-world-page)
+
+
+;; takes the tcp request and creates a reply
+;; input stream -> output stream
+(define (handle-tcp input-stream output-stream)
+  ; Discard the request header (up to blank line):
+  (regexp-match #rx"(\r\n|^)\r\n" input-stream)
+  ; Send reply:
+  (display hello-world-page output-stream))
+
 
 
 ;; the following runs the tests.  should be commented out
 ;; for production
-(test)
+;;(test)
