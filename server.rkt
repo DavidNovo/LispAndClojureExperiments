9bf9b4647c08b6e897b0e4b91de8c61bbffbf4a6
DavidNovo <david.novogrodsky@gmail.com>
2014-02-18 22:57:52 -0600

    adding memory control for handler and dispatcher

--- server.rkt
+++ server.rkt
@@ -52,9 +52,7 @@ HTTP/1.0
 (define sample-get-request
   "GET /path/file.html HTTP/1.0
 From: someuser@jmarshall.com
-User-Agent: HTTPTool/1.0
-
-")
+User-Agent: HTTPTool/1.0")
 ;; Template rules used:
 ;;  - atomic non-distinct: String
 ;; template
@@ -65,7 +63,7 @@ User-Agent: HTTPTool/1.0
 ;; tests
 (check-expect (string? hello-world-page) true)
 (check-expect (number? port-no) true)
-;;(check-expect (= port-no 8080) true)
+(check-expect (= port-no 8080) true)
 
 ;; functions 
 
@@ -96,6 +94,7 @@ User-Agent: HTTPTool/1.0
 ;;  ( listener))
 (define (accept-and-handle listener)
   (define cust (make-custodian))
+  (custodian-limit-memory cust (* 50 1024 1024))
   (parameterize ([current-custodian cust])
     (define-values (input-stream output-stream)(tcp-accept listener))
     ;;putting each connecton into its own thread
@@ -115,40 +114,107 @@ User-Agent: HTTPTool/1.0
 ;                          (tcp-listen 8081 1 #t )))
 ;               #t )
 
+
+
 ;; takes the tcp request and creates a reply
 ;; input stream, output stream  -> output stream
 (define (handle-tcp input-stream output-stream)
-  ; Discard the request header (up to blank line):
-  (regexp-match #rx"(\r\n|^)\r\n" input-stream)
-  ; Send reply:
-  (display hello-world-page output-stream))
+  (define request (check-request input-stream))
+  (when request
+    ; Discard the request header (up to blank line):
+    (regexp-match #rx"(\r\n|^)\r\n" input-stream)
+    ;dispatcher
+    (let ([xexpr (dispatch (list-ref  request 1))])
+      ; send reply
+      (display "HTTP/1.0 200 Okay\r\n" output-stream)
+      (display "Server: k\r\nContent-Type: text/html\r\n\r\n" output-stream)
+      (display (xexpr->string xexpr) output-stream))))
+
 ;; examples and tests
 ;; parameters for testing
-(check-expect (handle-tcp "null string" (current-output-port)) (display hello-world-page))
+(check-expect (handle-tcp (current-input-port) (current-output-port)) (display hello-world-page))
 
-;; !!
 ;; checking a request
 ;; HTTP request -> Boolean
 ;;(define request 
 ;;  (regexp-match "pattern to determine if it is a request"
 ;;                (read-line in)))  ;stub
 ;; function body
-(define (request in)
+(define (check-request in)
+  ;;Match the first line to extract the request:
   (regexp-match #rx"^GET (.+) HTTP/[0-9]+\\.[0-9]+"
                 (read-line in)))
 ;; tests
-(check-expect (request (open-input-string sample-get-request-bad))
-              #f)
-(check-expect (request (open-input-string sample-get-request))
+(check-expect (check-request (open-input-string sample-get-request-bad)) #f)
+(check-expect (check-request (open-input-string sample-get-request))
               '("GET /path/file.html HTTP/1.0" "/path/file.html"))
 
-;; !! a new wish
-;; URL -> string to create an html page
-;; this function takes a URL and returns a string
+
+;; URL -> function that creates a page
+;; this function takes a URL and returns a function
+;; based on the path of the incomming url
 ;;(define dispatch URL null) ;stub
+(define test-string "http://localhost:8080/")
+(define test-hello-page-url "http://localhost:8080/hello")
+(define test-hello-page-url-return `(html (body "Hello, World!")))
 
 
+(define (dispatch str-path) 
+  ;; Parse the request as a URL:
+  (define url (string->url str-path))
+  ;; Extract the path part:
+  (define path (map path/param-path (url-path url)))
+  ;; Find a handler based on the path's first element:
+  (define h (hash-ref dispatch-table (car path) #f))
+  (if h
+      ;; Call a handler:
+      (h (url-query url))
+      ;; No handler found:
+      `(html (head (title "Error"))
+             (body
+              (font ((color "red"))
+                    "Unknown page: "
+                    ,str-path)))))
 
+
+;; tests
+(check-expect (dispatch test-hello-page-url) test-hello-page-url-return)
+
+;;!! ok, need new function to build reply page
+;; string, destination url, hidden value on form -> reply html page
+;; use this url for testing http://localhost:8081/many
+
+(define (build-request-page label next-url hidden)
+  `(html
+    (head (title "Enter a Number to Add"))
+    (body ([bgcolor "white"])
+          (form ([action ,next-url] [method "get"])
+                ,label
+                (input ([type "text"] [name "number"]
+                        [value ""]))
+                (input ([type "hidden"] [name "hidden"]
+                        [value ,hidden]))
+                (input ([type "submit"] [name "enter"]
+                        [value "Enter"]))))))
+
+;; adding more elements to the hash dispatch table require
+;; function for the reply
+(define (many query)
+  (build-request-page "Number of greetings:" "/reply" ""))
+(define (reply query)
+  (define n (string->number (cdr (assq 'number query))))
+  `(html (body ,@(for/list ((i (in-range n)))
+                   "hello"))))
+
+;; hash table
+(define dispatch-table (make-hash))
+(hash-set! dispatch-table "hello"
+           (lambda (query)
+             '(html (body "Hello, World!"))))
+(hash-set! dispatch-table "many" many)
+(hash-set! dispatch-table "reply" reply)
+
+  
 ;; the following runs the tests.  should be commented out
 ;; for production
 (test)
